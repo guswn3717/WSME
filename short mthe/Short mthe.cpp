dbf8cd4be04c36771bd2fbf2c2788a59dc6b0c75
guswn3717 <rudals9973@gmail.com>
2025-01-30 15:53:07 +0900

    wshthc 20250130

---
+++ short mthe/Short mthe.cpp
@@ -0,0 +1,4 @@
+#include<iostream>
+#include<stdlib.h>
+#include<time.h>
+using namespace std;int main(){srand(time(0));int A=0,B=0;cin>>B;for(int C=0; C<B;C++)if(rand()%3==rand()%3);else{A++;}cout<<"s"<<A<<"f"<<B-A;}
